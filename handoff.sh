229d8c0bc25eb9a00e6cbf4c7029b2fd800134bd
Grant Zukel <zukeru@gmail.com>
2015-06-22 15:09:40 -0600

    new method of file delivery

--- handoff.sh
+++ handoff.sh
@@ -7,6 +7,7 @@ sudo wget https://dl.bintray.com/mitchellh/terraform/terraform_0.5.3_linux_amd64
 sudo unzip terraform_0.5.3_linux_amd64.zip
 sudo mkdir $WORKSPACE/configs
 sudo cp $WORKSPACE/Output.tf $WORKSPACE/terraform/configs/Output.tf
+sudo cp $WORKSPACE/user-data.txt $WORKSPACE/terraform/configs/user-data.txt 
 cat $WORKSPACE/terraform/configs/Output.tf
-cat $WORKSPACE/user-data.txt
+cat $WORKSPACE/terraform/configs/user-data.txt
 $WORKSPACE/terraform/terraform apply $WORKSPACE/terraform/configs
