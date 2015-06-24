2683e4092922741d57f281f86e5fb9122f8468d4
Grant Zukel <zukeru@gmail.com>
2015-06-24 11:37:22 -0600

    smart security group join

--- build_asg.py
+++ build_asg.py
@@ -269,7 +269,7 @@ def get_a_uuid():
     return r_uuid.replace('=', '')
 
 uuid = str(get_a_uuid())
-asg_name = asg_name + uuid[:8]
+asg_name = asg_name + '-' + uuid[:8] + '-' + role
 cluster_name = asg_name
 security_groups = security_groups.replace(' ', '')
 security_groups = build_security_group(security_groups, cluster_name, sg_tag)
